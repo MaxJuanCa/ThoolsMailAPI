e4845840915b2388680efedd56c7722f6f7279f9
Juan Camilo Ramirez Ortiz <=>
2020-04-20 09:54:55 -0500

    Ajustes post bloqueo Sendgrid

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b6998dd07c7e90fbec82";
+/******/ 	var hotCurrentHash = "cc8d30d80a82fb9d13cb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
