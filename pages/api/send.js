90a888809e1bbb141a81a6f087975797044ad68f
Juan Camilo Ramirez Ortiz <=>
2020-04-21 19:42:04 -0500

    Test

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,28 +1,23 @@
-require('dotenv').config();
-const sgMail = require('@sendgrid/mail');
+const sgMail = require('@sendgrid/mail')
 
-export default async function (req, res) {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+export default async function(req, res) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-    const {
-        email,
-        message
-    } = req.body
+  const { nombre, empresa, email } = req.body
 
-    const content = {
-        to: 'juankamilo@gmail.com',
-        from: 'jcramirez@thools.com',
-        subject: `New Message From - ${email}`,
-        text: message,
-        html: `<p>${message}</p>`
-    }
+  const content = {
+    to: 'juakamilo@gmail.com',
+    from: email,
+    subject: `New Message From - ${email}`,
+    text: empresa,
+    html: `<p>${nombre} - ${empresa}</p>`
+  }
 
-    try {
-        await sgMail.send(content)
-        res.status(200).send('Message sent successfully.')
-    } catch (error) {
-        console.log('ERROR', error)
-        res.status(400).send('Message not sent.')
-    }
+  try {
+    await sgMail.send(content)
+    res.status(200).send('Message sent successfully.')
+  } catch (error) {
+    console.log('ERROR', error)
+    res.status(400).send('Message not sent.')
+  }
 }
-
